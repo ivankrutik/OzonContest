2f3871ad37784f2c18e1e48d44748ddc5979f743
IvanKrutik <ivan.krutik@gmail.com>
2023-08-09 18:09:59 +0400

    Ozon_1

---
+++ Summator_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+var count = int.Parse(Console.ReadLine().ToString());
+var results = new List<int>();
+
+for (int i = 0; i < count; i++)
+{
+    var arr = Console.ReadLine().ToString().Split(' ');
+    results.Add(int.Parse(arr[0]) + int.Parse(arr[1]));
+}
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
